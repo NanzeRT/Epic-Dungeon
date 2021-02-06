cb3d8a72fc3d7a8979daf12ec4a948b5671a2f23
NanzeRT <nanotest.vlad@gmail.com>
2021-02-06 21:36:28 +0300

    quit + git files

---
+++ Assets/Scripts/Staff/exit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exit : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetAxis("Cancel") > 0)
+        {
+            Debug.Log("Quit");
+            Application.Quit();
+        }
+    }
+}
